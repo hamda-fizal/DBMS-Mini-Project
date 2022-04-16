436fd414f7ad91df1b78813dccdd477cb5a52d82
Hanna Sabu Mampallil <hanblessab@gmail.com>
2022-04-16 18:20:40 +0530

    home page ui

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ const app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
   res.render("index");
